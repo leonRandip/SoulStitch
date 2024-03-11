53ed9bf44531ddb58759aaed76d08d9bbf277598
leonRandip <leonrandip@gmail.com>
2024-03-11 15:03:22 +0530

    "data config"

--- src/components/DynamicPage/contact.js
+++ src/components/DynamicPage/contact.js
@@ -3,6 +3,6 @@ export const Contact = {
   header: "Contact Page",
   heading: "Lorem ipsum",
   content:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.",
+    "Lorem ipsum dolor sit amt, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi utaliquip ex ea commodo consequat. Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.",
   image: image,
 };
